c760aaad412b9d3d7f61c259f459d1befac84d0a
Frank Costoya <66138552+vinylflamingo@users.noreply.github.com>
2023-07-06 19:22:38 -0400

    kewl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
   newMemberName: string = "";
   members: string[] = [];
   errorMessage: string = "";
+  numberOfTeams: number = 0;
+  teams: string[][] = [];
+  
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
+
+  onNumbersOfTeamsInput(numberOfTeams: string) {
+    this.numberOfTeams = parseInt(numberOfTeams);
+  }
 
   addMember() {
     if (!isNullOrEmpty(this.newMemberName)) {
@@ -25,8 +35,30 @@ export class AppComponent {
     }
   }
 
-  onInput(member: string) {
-    this.newMemberName = member;
+  generateTeams() {
+    if (this.numberOfTeams <= 0) {
+      this.errorMessage = "Please enter a number of teams greater than 0";
+    } else { 
+      this.errorMessage = "";
+    }
+
+    const allMembers = [...this.members]
+
+    while(allMembers.length) {
+      for(let i = 0; i < this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const randomMember = allMembers.splice(randomIndex, 1)[0];
+  
+        if(this.teams[i]){
+          this.teams[i].push(randomMember);
+        }
+        else {
+          this.teams[i] = [randomMember];
+        }
+  
+        console.log(this.teams);
+      }
+    }
   }
 
 }
